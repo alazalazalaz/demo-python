6e7f8635e9d67929de943d2077566d1658c419a9
张雄 <1493943799@qq.com>
2021-03-02 00:23:25 +0800

    滑块识别

--- demo_identify_code/slider_code.py
+++ demo_identify_code/slider_code.py
@@ -1,29 +1,28 @@
 import cv2
-# cv2 图像处理库
-# pip install opencv-python
+import numpy as np
 
-index = 2
+# #  cv图片用数组表示方法：
+# #  从左到右，从上到下这样开始表示的[ [y0x0, y0x1...y0xx] [y1x0, y1x1...y1xx]... [yyx0, yyx1...yyxx] ]
+# #  其中y0x1~yyxx等每一个像素点又是一个数组，即色彩rgb表示法， 比如[255 255 255]表示白色
+# test_name = "./img/test_1x2.png"  # 1x2pix的图片，也就是2个点
+# test_handle = cv2.imread(test_name)     # imread()函数，把图片转换为一个大的数组
+# print(test_handle)  # [ [[58 56 54]] [[58 56 54]] ]表示两个点，[58 56 54]表示一个点
+# exit()
+
+
+index = 1
+
 bg_img = cv2.imread("./img/cap_background_{}.jpeg".format(index))
-front_img = cv2.imread("./img/cap_front_{}.png".format(index))
+font_img = cv2.imread("./img/cap_front_{}.png".format(index))
+targ = './img/targ.jpg'
 
-bg_edge = cv2.Canny(bg_img, 100, 500)   # 识别图片边缘
-front_edge = cv2.Canny(front_img, 100, 500)
-
-bg_pic = cv2.cvtColor(bg_edge, cv2.COLOR_GRAY2RGB)  # 转换图片格式
-front_pic = cv2.cvtColor(front_edge, cv2.COLOR_GRAY2RGB)
-
-# 缺口匹配
-res = cv2.matchTemplate(bg_pic, front_pic, cv2.TM_CCOEFF_NORMED)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-x = max_loc[0]
-y = max_loc[1]
-print(min_val, max_val, min_loc, max_loc)
-
-# 绘制
-th, tw = bg_pic.shape[:2]
-tl = max_loc    # 左上角坐标
-br = (tl[0]+10, tl[1]+10)   # 右下角坐标
-cv2.rectangle(bg_img, tl, br, (0, 0, 255), 2)   # 绘制矩形(图片句柄，左上角，右下角，颜色，宽度)
+bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2GRAY)
+bg_img = abs(255 - bg_img)
+cv2.imwrite(targ, bg_img)
+bg_img = cv2.imread(targ)
+result = cv2.matchTemplate(bg_img, font_img, cv2.TM_CCOEFF_NORMED)
+y, x = np.unravel_index(result.argmax(), result.shape)
+# 展示圈出来的区域
+print("识别结果： x:{} y:{}".format(x, y))
+cv2.rectangle(bg_img, (x, y), (x + 10, y + 10), (7, 249, 151), 2)
 cv2.imwrite("./img/cap_background_result_{}.jpg".format(index), bg_img)
-
